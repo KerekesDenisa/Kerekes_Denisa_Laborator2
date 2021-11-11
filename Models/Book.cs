a54f8d1a509fd6f02ffd35151a101236913189d9
Kerekes Denisa <denisa.camelia0717@gmail.com>
2021-11-11 21:17:39 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kerekes_Denisa_laborator2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
